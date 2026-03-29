08b28c9f1977af7bd0d66c10305e2ac47aed869a
kozaczini <j.kozak046@gmail.com>
2026-03-29 03:53:55 +0200

    dodanie plikow

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,7 +12,12 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // 4. Rejestracja bazy danych MS SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString,
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null)
+        ));
 // 5. Konfiguracja CORS - pozwala Reactowi (port 8080) na dostęp do API
 builder.Services.AddCors(options => {
     options.AddDefaultPolicy(policy => {
